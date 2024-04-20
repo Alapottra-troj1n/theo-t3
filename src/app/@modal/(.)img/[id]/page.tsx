bbc57be0da5f93b0639239060414a33d64e53650
unknown <alapottratrojon@gmail.com>
2024-04-21 00:15:43 +0600

    modal added

--- src/app/@modal/(.)img/[id]/page.tsx
+++ src/app/@modal/(.)img/[id]/page.tsx
@@ -1,4 +1,5 @@
 import { getPhoto } from "~/server/queries";
+import { Modal } from "./modal";
 
 export default async function PhotoModal({
   params: { id: photoId },
@@ -6,7 +7,9 @@ export default async function PhotoModal({
   params: { id: string };
 }) {
   const photo = await getPhoto(photoId);
-  return <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-    <img src={photo.url} alt="" />
-  </div>;
+  return (
+    <Modal>
+      <img src={photo.url} alt="" />
+    </Modal>
+  );
 }
